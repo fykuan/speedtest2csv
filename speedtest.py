2257b4e9fe1c9761c038846fe6acfda57a44f045
Fan-Yu Kuan <fykuan@gmail.com>
2014-11-20 13:26:43 +0800

    speedtest-cli 用絕對路徑

--- speedtest.py
+++ speedtest.py
@@ -6,7 +6,7 @@ import time
 
 # 總是使用 2189 這台 speedtest server
 # 2189) TFN Media Co., Ltd. (Kaohsiung, Taiwan) [15.05 km]
-p = subprocess.Popen(["speedtest-cli", "--simple", "--server", "2189"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p = subprocess.Popen(["/usr/local/bin/speedtest-cli", "--simple", "--server", "2189"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 (out, err) = p.communicate()
 
