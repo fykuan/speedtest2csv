4911a22f1b20e69c1fad9b9dbaa5f8e6da902f11
Fan-Yu Kuan <fykuan@gmail.com>
2014-12-30 14:29:22 +0800

    handling exceptions

--- speedtest2csv.py
+++ speedtest2csv.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-import subprocess
 import re
+import subprocess
+import sys
 import time
 
-p = subprocess.Popen(["/usr/local/bin/speedtest-cli"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+try:
+    p = subprocess.Popen(["/usr/local/bin/speedtest-cli"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except:
+    print "speedtest failed.."
+    sys.exit(0)
 
 (out, err) = p.communicate()
 
@@ -18,8 +23,12 @@ currTime =  time.strftime("%c")
 # 把 remoteHost 可能出現的逗號去掉
 remoteHostStr = remoteHost[0].replace(",", "")
 
-f = open('speedtest_%s.csv' % time.strftime("%Y%m%d"), 'a+')
-f.write('%s,%s,%s,%s,%s,%s\r\n' % (currTime, localIP[0], remoteHostStr, pingResult[0], downloadResult[0], uploadResult[0]))
-f.close()
+try:
+    f = open('speedtest_%s.csv' % time.strftime("%Y%m%d"), 'a+')
+    f.write('%s,%s,%s,%s,%s,%s\r\n' % (currTime, localIP[0], remoteHostStr, pingResult[0], downloadResult[0], uploadResult[0]))
+    f.close()
+    print "Ping: %s ms, IP: %s, Remote Host: %s, Download: %s Mbits/s, Upload %s Mbits/s" % (pingResult[0], localIP[0], remoteHostStr, downloadResult[0], uploadResult[0])
+except:
+    print "write log file failed.."
+    sys.exit(0)
 
-print "Ping: %s ms, IP: %s, Remote Host: %s, Download: %s Mbits/s, Upload %s Mbits/s" % (pingResult[0], localIP[0], remoteHostStr, downloadResult[0], uploadResult[0])
